a9a2e7a0c8455adc948ed2d4c743b9eae5723d55
Samuel Toompas <samueltoompas@gmail.com>
2025-08-26 14:51:33 -0700

    Adds get_point function for random access into array

--- src/image.py
+++ src/image.py
@@ -7,8 +7,7 @@ class image_array:
     Class to hold a pixel matrix
     """
 
-    def __init__(self, path):
-
+    def __init__(self, path:str) -> None:
         # Initialize variables
         self.data = []
 
@@ -20,19 +19,27 @@ class image_array:
         self.width  = image.shape[1]
         color       = image.shape[2]
         y_coords, x_coords = np.meshgrid(
-                np.arange(self.width), 
-                np.arange(self.height), 
-                indexing="ij"
-                )
+            np.arange(self.width), 
+            np.arange(self.height), 
+            indexing="ij"
+            )
 
         pixels = np.column_stack((
-                x_coords.ravel(), 
-                y_coords.ravel(), 
-                image.reshape(-1, color)
-                ))
+            x_coords.ravel(), 
+            y_coords.ravel(), 
+            image.reshape(-1, color)
+            ))
 
         pixels_list = pixels
         self.data = pixels_list
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.data)
+
+    def get_point(self, x:int, y:int) -> list[int]:
+        """
+        random access for a specific point
+        """
+        # TODO make this the array index operation
+
+        return self.data[x + y * width]
